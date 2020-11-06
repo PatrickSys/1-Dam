628d0b00b41a4ed9fe885f6272b3a19222c049cb
PatrickSys <rossellocolompatrick@gmail.com>
2020-11-06 18:42:25 +0100

    ej1

---
+++ src/excs.java
@@ -0,0 +1,17 @@
+public class excs {
+
+    public static void main(String[] args) {
+        {
+            System.out.println("Els múltiples de 5 són");
+        }
+
+
+            for (int nombre = 0; nombre <100; nombre++) {
+                if ((nombre%5)==0)
+                {
+                    //System.out.println(nombre%5);
+                    System.out.println(nombre );
+                }
+            }
+    }
+}
