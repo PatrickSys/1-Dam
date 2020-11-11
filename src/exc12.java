6d8fbe0313f966ed11c4b2773be456692afc4668
PatrickSys <rossellocolompatrick@gmail.com>
2020-11-11 18:47:29 +0100

    yes

--- src/exc12.java
+++ src/exc12.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class exc12 {
 
     public static void main(String[] args) {
+        System.out.println("Introdueix el nº de xifres que voleu conéixer");
         Scanner scanner=new Scanner(System.in);
         int vegades=scanner.nextInt();
         int x=0;
@@ -10,14 +11,12 @@ public class exc12 {
             int x2=x+x1;
             x=x1;
             x1=x2;
-            if (i==2){
-                System.out.println(x-x);
+            if (i==2) {
+                System.out.println(0);
                 System.out.println(x1);
                 System.out.println(x2);
             }
-            else {
-                System.out.println(x2);
-            }
+            else System.out.println(x2);
         }
 
     }
