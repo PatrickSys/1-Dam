2b886f2f8250253f045d9e81a2acff313a7f9f04
PatrickSys <prossello@nigul.cide.es>
2020-11-16 22:29:03 +0100

    updATE

--- src/Bucles/exc12bucles.java
+++ src/Bucles/exc12bucles.java
@@ -1,5 +1,7 @@
+package Bucles;
+
 import java.util.Scanner;
-public class exc12 {
+public class exc12bucles {
 
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
@@ -7,18 +9,13 @@ public class exc12 {
         int x=0;
         int x1=1;
         System.out.println("introdueix el la quantitat de dígits que vols conéixer");
-        for (int i = 2; i<vegades; i++){
-            int x2=x+x1;
-            x=x1;
+        for (int i = 0; i<vegades; i++){
+            int x2=x;
+            x=x1+x;
             x1=x2;
-            if (i==2){
-                System.out.println(x-x);
+
                 System.out.println(x1);
-                System.out.println(x2);
-            }
-            else {
-                System.out.println(x2);
-            }
+
         }
 
     }
